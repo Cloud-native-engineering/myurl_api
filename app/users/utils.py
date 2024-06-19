da49b081c65abe537f2c12efcce6d81eb3de2a8f
Yves <yves@wlankabel.ch>
2024-06-19 09:56:58 +0200

    Dev (#10)

--- app/users/utils.py
+++ app/users/utils.py
@@ -1,5 +1,7 @@
 from app.models.user import User
 from app.utils import json_abort
+from random_username.generate import generate_username
+from app.extensions import db
 
 ####
 ## Utility Functions about Users
@@ -12,8 +14,20 @@ def get_user_by_id(user_id):
 
 def get_user_by_auth0_id(auth0_id):
     user = User.query.filter_by(auth0_id=auth0_id).first()
+        # If the user doesn't exist, create a new one
     if user is None:
-        json_abort(404, {"error": "User not found", "identifier": auth0_id})
+        # Generate random username
+        username = generate_username()[0]
+        
+        # Try to get the user by username
+        while User.query.filter_by(username=username).first() is not None:
+            # If the username already exists, generate a new one
+            username = generate_username()[0]
+
+        user = User(auth0_id=auth0_id, username=username)
+        db.session.add(user)
+        db.session.commit()
+
     return user
 
 def get_user_by_username(username):
